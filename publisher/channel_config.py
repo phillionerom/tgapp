adf59a035a31ce210febeb620b1752d77557f134
Juan Moreno <philli.onerom@gmail.com>
2025-07-11 16:39:54 +0200

    clean old files

--- publisher/channel_config.py
+++
@@ -1,9 +0,0 @@
-CATEGORY_TO_CHANNELS = {
-    "tecnologia": ["@ofertas_tech"],
-    "moda": ["@ofertas_moda"],
-    "bebe": ["@ofertas_bebe"],
-    "default": ["@D_o_n_Oferton"]
-}
-
-def get_channels_for_category(category: str) -> list[str]:
-    return CATEGORY_TO_CHANNELS.get(category, CATEGORY_TO_CHANNELS["default"])
