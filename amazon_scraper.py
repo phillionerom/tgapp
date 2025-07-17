468dcf6292a833e0d60c2c2556c65a8c3d8c4909
Juan Moreno <philli.onerom@gmail.com>
2025-07-17 17:56:19 +0200

    reenable ignore http errors

--- amazon_scraper.py
+++ amazon_scraper.py
@@ -96,7 +96,7 @@ async def get_amazon_product_data(product_url: str) -> dict:
                     user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
                     locale="es-ES",
                     viewport={"width": 1280, "height": 800},
-                    #ignore_https_errors=True
+                    ignore_https_errors=True
                 )
                 page = await context.new_page()
 
